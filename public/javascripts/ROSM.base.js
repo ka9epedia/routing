37601ddcf7df4047a4ce0006d1a6e57511c01c33
Kage <42766115+ka9epedia@users.noreply.github.com>
2018-12-18 14:23:30 +0900

    Update ROSM.base.js

--- public/javascripts/ROSM.base.js
+++ public/javascripts/ROSM.base.js
@@ -13,4 +13,3 @@ ROSM.CONSTANTS.VIA_LABEL = "案内";
 
 ROSM.DEFAULTS.ZOOM_LEVEL = 14;
 ROSM.DEFAULTS.HOST_GEOCODER_URL = "http://nominatim.openstreetmap.org/search";
-//ROSM.DEFAULTS.HOST_GEOCODER_URL = "http://maps.googleapis.com/maps/api/geocode/";
